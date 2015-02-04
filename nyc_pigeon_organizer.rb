2252f175f0c8895625abf25cea7cdd617def25fe
axton21 <axton_n@yahoo.com>
2015-02-04 17:57:18 -0500

    yay

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  birdhash = {}
+  pigeon_list = data.each do |pigeonhash,stat|
+    #pigeonhash = :color :gender :lives  stat = purple :male :subway
+    stat.each do | attribute, birdname|
+     #attribute = purple,male, subway   birdname = ["theo", "peter"]
+      birdname.each do |bird|
+        if birdhash[bird] == nil
+          birdhash[bird] = {}
+        end
+        if birdhash[bird][pigeonhash] == nil
+          birdhash[bird][pigeonhash] = []
+        end
+        birdhash[bird][pigeonhash] << attribute.to_s
+        # binding.pry
+      end
+     # binding.pry
+    end 
+  end
+  birdhash
 end
